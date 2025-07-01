a4a6cdd9422a3fe6b4cd8f2c755d7b6f01129b19
Francesco Mazzitelli <82218677+FrancescoMazzitelli@users.noreply.github.com>
2025-07-01 13:35:31 +0200

    Update customembedding.py

--- customembedding.py
+++ customembedding.py
@@ -1,18 +1,30 @@
-from typing import TypeVar, Union
-from langchain_community.embeddings import HuggingFaceEmbeddings as HFE
+from typing import TypeVar, Union, List
+from sentence_transformers import SentenceTransformer
 from langchain.docstore.document import Document
+from langchain.embeddings.base import Embeddings
 
-class CustomEmbedding:
+class CustomEmbedding(Embeddings):
     Embeddable = Union[Document]
     D = TypeVar("D", bound=Embeddable, contravariant=True)
 
     def __init__(self):
-        self.hugging_face_model = HFE()
+        self.sentence_transformer_model = SentenceTransformer(model_name_or_path='intfloat/multilingual-e5-large-instruct', device='cuda')
 
     def __call__(self, input: D):
-        embedding = self.hugging_face_model.embed_query(str(input))
+        embedding = self.sentence_transformer_model.encode(str(input), convert_to_tensor=True, normalize_embeddings=True)
         return [float(x) for x in embedding]
 
     def get_text_embedding(self, input: D):
-        embedding = self.hugging_face_model.embed_query(str(input))
+        embedding = self.sentence_transformer_model.encode(str(input), convert_to_tensor=True, normalize_embeddings=True)
+        return [float(x) for x in embedding]
+
+    def embed_documents(self, documents: List[Document]):
+        embeddings = []
+        for doc in documents:
+            embedding = self.sentence_transformer_model.encode(str(doc), convert_to_tensor=True, normalize_embeddings=True)
+            embeddings.append([float(x) for x in embedding])
+        return embeddings
+
+    def embed_query(self, query: str):
+        embedding = self.sentence_transformer_model.encode(query, convert_to_tensor=True, normalize_embeddings=True)
         return [float(x) for x in embedding]
